f56999e2009edf560c77046001ef7f399185498e
Moritz Heidkamp <moritz@doo.net>
2012-03-13 12:28:40 +0100

    initial commit

---
+++ project.clj
@@ -0,0 +1,7 @@
+(defproject lein-hiccster "1.0.0-SNAPSHOT"
+  :description "A hiccup page server plugin for Leiningen"
+  :dependencies [[org.clojure/clojure "1.3.0"]
+                 [hiccup "1.0.0-beta1"]
+                 [ns-tracker "0.1.1"]
+                 [ring "1.0.2"]]
+  :eval-in-leiningen true)
---
+++ src/leiningen/hiccster.clj
@@ -0,0 +1,77 @@
+(ns leiningen.hiccster
+  (:use ns-tracker.core
+        hiccup.core
+        ring.middleware.file
+        ring.middleware.file-info
+        ring.util.response
+        ring.adapter.jetty)
+  (:require [swank.swank :as swank]))
+
+(def ^{:dynamic true}
+  *modified-namespaces* nil)
+
+(def *pages* (atom #{}))
+
+(defn- log [& msg]
+  (println (str "[" (java.util.Date.) "]")
+           (apply str msg)))
+
+(defn- reload-modified-namespaces []
+  (doseq [ns-sym (*modified-namespaces*)]
+    (log "loading " ns-sym)
+    (require ns-sym :reload)
+    (let [ns      (find-ns ns-sym)
+          publics (and ns (ns-publics ns))
+          page    (get publics 'page)]
+      (if (and page (fn? @page))
+        (swap! *pages* conj ns-sym)
+        (swap! *pages* disj ns-sym)))))
+
+(defn- html-response [page]
+  (-> (html page)
+      (response)
+      (content-type "text/html; charset=UTF-8")))
+
+(defn- page-index []
+  (html-response
+   [:html [:head [:title "hiccster page index"]]
+    [:body
+     [:h1 "pages"]
+     [:ul (map (fn [page]
+                 [:li [:a {:href (str "/" page)}
+                       (str page)]])
+               (deref *pages*))]]]))
+
+(defn- handle-page [req]
+  (reload-modified-namespaces)
+  (let [ns-sym (->> (.substring (:uri req) 1)
+                    (symbol))
+        ns (find-ns ns-sym)]
+    (let [page (and ns (ns-resolve ns-sym 'page))]
+      (cond page
+            (-> (page)
+                (html-response))
+            (= (:uri req) "/")
+            (page-index)
+            :else
+            (-> (response (str "var " ns-sym "/page not found"))
+                (status 404))))))
+
+(defn- wrap-logging [handler]
+  (fn [req]
+    (log (:uri req))
+    (handler req)))
+
+(def handler
+  (-> handle-page
+      (wrap-file "static")
+      (wrap-file-info)
+      (wrap-logging)))
+
+(defn hiccster
+  ([] (hiccster "src"))
+  ([& args]
+     (alter-var-root (var *modified-namespaces*)
+                     (constantly (ns-tracker args 0)))
+     (reload-modified-namespaces)
+     (run-jetty #'handler {:port 8765})))
